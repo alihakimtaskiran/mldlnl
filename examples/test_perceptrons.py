6a277ac497d0b72de1a2b0c1aeea5b46552d3fa7
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2019-11-19 19:26:34 +0200

    Update test_perceptrons.py

--- examples/test_perceptrons.py
+++ examples/test_perceptrons.py
@@ -4,12 +4,14 @@ mnist=input_data.read_data_sets("data/mnist",one_hot=True)
 
 nn=mldlnl.Perceptron([784,256,10],"relu")
 nn.fit(mnist.train.images,mnist.train.labels,epochs=10,lr=0.001,keep_prob=0.5)
+loss,acc=nn.test(mnist.test.images,mnist.test.labels)
+print("Test loss="+str(loss)," accuracy="+str(acc))
 
 print(nn.calc(mnist.test.images[7:10],argmax=True))
-print(nn.calc(mnist.test.images[7:10],argmax=False))
 nn.save("perceptron")
 
 p=mldlnl.Perceptron([784,256,10],"relu")
+print(p.calc(mnist.test.images[7:10],argmax=True))
 p.restore("perceptron")
 print(p.calc(mnist.test.images[7:10],argmax=True))
 
